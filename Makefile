6bdeca66bd9ca0f41b4e9065f91082cace0fb6f6
Kya Suzuki <kya@dhcp-10-105-46-230.wireless.northwestern.private>
2017-02-25 16:44:17 -0600

    fixed most errors besides Makefile

--- Makefile
+++ Makefile
@@ -1,15 +1,21 @@
-CXXFLAGS = -std=gnu++0x -Wall
+CXXFLAGS = -std=gnu++14 -Wall
 LDLIBS = -lstdc++
+
 .PHONY: all clean test
+
 all: webots-client
-clean: \
--rm webots-client *.o \
-cd test; $(MAKE) clean
-test: \
-cd test; $(MAKE) test
-test server: \
-node example-server.js
-node_modules: \
-npm install
-webots-client: webots.client.o //do i need to include easywsclient files
+
+clean:
+	-rm webots-client *.o
+
+test_server:
+	node example-server.js
+
+node_modules:
+	npm install
+
+webots-client: webots-client.o walk-client.o
+easywsclient: easywsclient.o
 webots-client.o: webots-client.cpp webots-client.h
+walk-client.o: walk-client.cpp walk-client.h
+easywsclient.o: easywsclient.cpp easywsclient.hpp
--- webots-client.cpp
+++ webots-client.cpp
@@ -1,4 +1,3 @@
-
 //websocket
 #include "easywsclient.hpp" // <-- include only if you don't want compile separately
 //#include "easywsclient.cpp"
@@ -19,7 +18,6 @@
 
 //using namespace webots;
 
-using namespace std;
 static easywsclient::WebSocket::pointer ws = NULL;
 
 // "WALK 5.8 30.0"
@@ -27,41 +25,17 @@ static easywsclient::WebSocket::pointer ws = NULL;
 // "STOPWALK"
 // stopWalking();
 
-void handle_message(const std::string & message)
-{
-  
-  size_t curr = 0; // the cursor
-  string c (""); // the current message
-  printf(">>> %s\n", message.c_str());
-
-  while (curr < message.length()){
-      if (message[curr] != ' '){
-          c.append(message[curr]);
-          curr++;
-      } else{
-          if (c == "WALK"){
-             string r = message.substr(curr + 1,message.length()); //remaining string
-             handle_walk(r);
-          }
-          if (c == "STOPWALK"){
-              stopWalking();
-              ws->close();
-              break;
-              }
-          }
-  }
-}
 
 
-void handle_walk(const string & r){
+void handle_walk(const std::string & r){
     size_t curr2 = 0;
-    string c2 = ""; // the current message in this loop
+    std::string c2 = ""; // the current message in this loop
     double speed = 0.0;
     double angle = 0.0;
-    string r2; 
+    std::string r2; 
     while (curr2 < r.length()){
         if (r[curr2] != ' '){
-            c2.append(m[curr2]);
+            c2 += r[curr2];
             curr2++;
         } else{
             speed = stod(c2);
@@ -75,9 +49,36 @@ void handle_walk(const string & r){
 }
  
 
+void handle_message(const std::string & message)
+{
+  
+  size_t curr = 0; // the cursor
+  std::string c (""); // the current message
+  printf(">>> %s\n", message.c_str());
+
+  while (curr < message.length()){
+      if (message[curr] != ' '){
+          c += message[curr];
+          curr++;
+      } else{
+          if (c == "WALK"){
+             std::string r = message.substr(curr + 1, message.length()); //remaining string
+             handle_walk(r);
+          }
+          if (c == "STOPWALK"){
+              stopWalking();
+              ws->close();
+              break;
+              }
+          }
+  }
+}
+
+
+
 int main()
 {
-using easywsclient::WebSocket;
+
 #ifdef _WIN32
   INT rc; 
   WSADATA wsaData;
@@ -89,12 +90,12 @@ using easywsclient::WebSocket;
   }   
 #endif
 
-  ws = WebSocket::from_url("ws://localhost:8126/foo");
+  ws = easywsclient::WebSocket::from_url("ws://localhost:8126/foo");
   assert(ws);
   ws->send("command STOPWALK recieved");
   ws->send("command WALK recieved");
   ws->send("hello");
-  while (ws->getReadyState() != WebSocket::CLOSED) {
+  while (ws->getReadyState() != easywsclient::WebSocket::CLOSED) {
     ws->poll();
     ws->dispatch(handle_message);
   }   
